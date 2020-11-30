2c4c17134ac2c6aaaab4e7a4ad593f8dde1fd7df
YEHOR <yehor.polyschuk@get-code.net>
2020-11-30 16:17:39 +0200

    file downloading stub added

--- frontend/src/components/manager/Reports.vue
+++ frontend/src/components/manager/Reports.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>reports here</div>
+  <div>
+    reports here
+    <div @click="downloadReport">DOWNLOAD</div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    downloadReport() {
+      console.log("downloading...");
+      const url = this.$globals.remlib_api_host + "reports/1";
+      const method = "GET";
+
+      this.$axios
+        .request({
+          url,
+          method,
+          responseType: "blob",
+        })
+        .then(function (response) {
+          const downloadUrl = window.URL.createObjectURL(
+            new Blob([response.data])
+          );
+          const link = document.createElement("a");
+          link.href = downloadUrl;
+          link.setAttribute("download", "report.pdf"); //any other extension
+          document.body.appendChild(link);
+          link.click();
+          link.remove();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
